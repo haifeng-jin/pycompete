0349c15d8a145398f2a2a6e63f0b1ce0062aa063
Haifeng Jin <jhfjhfj1@gmail.com>
2019-10-26 20:05:16 -0500

    Update trie.py

--- trie.py
+++ trie.py
@@ -29,3 +29,24 @@ class TrieNode(object):
                 return False
             cur = cur.leaves[c]
         return True
+    
+class Solution(object):
+    def longestWord(self, words):
+        Trie = lambda: collections.defaultdict(Trie)
+        trie = Trie()
+        END = True
+
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie)[END] = i
+
+        stack = trie.values()
+        ans = ""
+        while stack:
+            cur = stack.pop()
+            if END in cur:
+                word = words[cur[END]]
+                if len(word) > len(ans) or len(word) == len(ans) and word < ans:
+                    ans = word
+                stack.extend([cur[letter] for letter in cur if letter != END])
+
+        return ans
